c843a114e98a2109ce92c5bc937a16889271ef7b
ebubekeyz <ebubeofforjoe@gmail.com>
2024-05-17 21:32:08 +0100

    25

--- src/components/DashboardCol1.jsx
+++ src/components/DashboardCol1.jsx
@@ -86,7 +86,7 @@ const DashboardCol1 = () => {
                 {item.package.map((item2) => {
                   const { days } = item2;
                   return (
-                    <div className="flex items-center gap-x-5">
+                    <div className="flex-col lg:flex items-center gap-x-5">
                       <span className="text-red-400">Exp:</span>
                       <div className="rounded-full bg-green-600 h-3 w-3"></div>
                       {moment(updatedAt).add(days, 'days').calendar()}
@@ -124,7 +124,7 @@ const DashboardCol1 = () => {
           {Object.values(withdraw).map((item) => {
             const { updatedAt } = item;
             return (
-              <div className="flex gap-x-5 items-center">
+              <div className="flex-col lg:flex gap-x-5 items-center">
                 <span className="text-pink-500">Date:</span>
                 <div className="rounded-full bg-green-600 h-3 w-3"></div>
                 {moment(updatedAt).calendar()}
--- src/components/DashboardCol2.jsx
+++ src/components/DashboardCol2.jsx
@@ -52,7 +52,7 @@ const DashboardCol2 = () => {
           {Object.values(referrals).map((item) => {
             const { updatedAt } = item;
             return (
-              <div className="flex gap-x-5 items-center">
+              <div className="flex-col lg:flex gap-x-5 items-center">
                 <span className="text-orange-600">Joined:</span>
                 <div className="rounded-full bg-green-600 h-3 w-3"></div>
                 {moment(updatedAt).calendar()}
